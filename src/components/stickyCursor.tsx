720b462966a495d0851b0115f9c8603320cbea70
lucas.rimbault <lucas.rimbault@tentelemed.com>
2024-06-26 15:13:51 +0200

    homepage - fix vercel build

--- src/components/stickyCursor.tsx
+++ src/components/stickyCursor.tsx
@@ -7,7 +7,7 @@ const StickyCursor = () => {
         y: useMotionValue(0)
     }
 
-    const manageMouseMove = (e) => {
+    const manageMouseMove = (e: MouseEvent) => {
         const cursorSize = 20
         const { clientX, clientY } = e;
         mouse.x.set(clientX - cursorSize / 2);
